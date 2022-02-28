e2feaf70627a5a240a64b93c82b737b604263def
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-02-28 19:04:16 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,10 @@
-url=https://ctldevopsdemo.jfrog.io/artifactory/default-maven-virtual/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/LoginWebApp-1.0-SNAPSHOT.war
-wget --user=nileshchakkar@gmail.com --password=Qkiosk@9031 -q -N $url
+#!/bin/bash
+systemctl stop tomcat
+url=https://devops2022.jfrog.io/artifactory/default-maven-local/CrudDemoWithMySql/CrudDemoWithMySql/0.0.1-SNAPSHOT/CrudDemoWithMySql-0.0.1-SNAPSHOT.war
+wget --user=ctldevopsnov2021@gmail.com --password=Devops@2022 -q -N $url
 rm -rf /tmp/artifacts
 mkdir /tmp/artifacts
-mv LoginWebApp-1.0-SNAPSHOT.war LoginWebApp.war
-cp LoginWebApp.war /tmp/artifacts
-cp /tmp/artifacts/LoginWebApp.war /usr/share/tomcat/webapps
-sudo systemctl stop tomcat
+mv CrudDemoWithMySql-0.0.1-SNAPSHOT.war CrudDemoWithMySql.war
+cp CrudDemoWithMySql.war /tmp/artifacts
+cp /tmp/artifacts/CrudDemoWithMySql.war /usr/share/tomcat/webapps
 sudo systemctl start tomcat
